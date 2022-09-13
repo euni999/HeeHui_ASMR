238dc6ac1bf59725e360a009806e499c50a8572e
euni999 <jeoneunhee322@gmail.com>
2022-09-13 15:35:09 +0900

    connect

--- src/main/java/com/youtube_api/data/Search.java
+++ src/main/java/com/youtube_api/data/Search.java
@@ -15,20 +15,12 @@ import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.*;
 import com.google.gson.JsonArray;
 import com.youtube_api.model.SearchItem;
-import org.json.simple.parser.ParseException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 
