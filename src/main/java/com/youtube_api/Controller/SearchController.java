ae78260e00cf3709c51ee603375fb251ed7119ac
euni999 <jeoneunhee322@gmail.com>
2022-10-16 22:03:27 +0900

    DB connect

--- src/main/java/com/youtube_api/Controller/SearchController.java
+++ src/main/java/com/youtube_api/Controller/SearchController.java
@@ -1,18 +1,8 @@
 package com.youtube_api.Controller;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.api.client.json.Json;
-import com.google.gson.JsonArray;
 import com.youtube_api.data.Search;
 import com.youtube_api.model.SearchItem;
-import com.youtube_api.model.Video;
-import com.youtube_api.repository.VideoRepository;
-import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.json.JSONParser;
-import org.h2.util.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,7 +26,6 @@ public class SearchController {
         int max = Integer.parseInt(items);
         List<SearchItem> item = Search.youTubeSearch(search, max);
         ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(new File("tingle.json"), item);
         String result = mapper.writeValueAsString(item);
         System.out.println(result);
         return result;
